e88a8d238c545b79018407141f67e4d140f6d32b
Kyle Perri <45472111+Kbperri@users.noreply.github.com>
2019-11-07 15:32:25 -0800

    Initial Commit

---
+++ CreateSDFile.py
@@ -0,0 +1,82 @@
+import arcpy
+import json
+
+def CreateSDFile(configFile):
+    try:
+        config = json.load(configFile)
+    except:
+        arcpy.AddMessage("Failed to load config.")
+        print("Failed to load config.")
+    
+    try:
+        toolbox = config["toolbox"]
+        alias = config["alias"]
+        toolName = config["toolName"]
+        toolArgs = config["toolArgs"]
+        sddraft = config["sdDraft"]
+        sd = config["sd"]
+        portalurl = config["portalURL"]
+        portalusername = config["portalUsername"]
+        portalpassword = config["portalPassword"]
+        serverURL = config["serverURL"]
+        serviceName = config["serviceName"]
+        serverType = config["serverType"]
+        connectionFilePath = config["connectionFilePath"]
+        copyDataToServer = config["copyDataToServer"]
+        folderName = config["folderName"]
+        summary = config["summary"]
+        tags = config["tags"]
+        executionType = config["executionType"]
+        resultMapServer = config["resultMapServer"]
+        showMessages = config["showMessages"]
+        maximumRecords = config["maximumRecords"]
+        minInstances = config["minInstances"]
+        maxInstances = config["maxInstances"]
+        maxUsageTime = config["maxUsageTime"]
+        maxWaitTime = config["maxWaitTime"]
+        maxIdleTime = config["maxIdleTime"]
+        constantValues = config["constantValues"]
+    except KeyError as keyErr:
+        arcpy.AddMessage(f"Config file missing value: {keyErr}")
+        print(f"Config file missing value: {keyErr}")
+    except Exception as e:
+        arcpy.AddMessage(f"Error occured in retreiving config values: {e}")
+        print(f"Error occured in retreiving config values: {e}")
+
+    arcpy.AddMessage("Starting.")
+    arcpy.ImportToolbox(toolbox, "SDCreation")
+    arcpy.AddMessage(arcpy.GetMessages(0))
+    customToolMethod = getattr(arcpy, f"{toolName}_{alias}")
+    result = customToolMethod(*toolArgs)
+    arcpy.AddMessage(arcpy.GetMessages(0))
+    arcpy.SignInToPortal(portalurl, portalusername, portalpassword)
+    arcpy.AddMessage(arcpy.GetMessages(0))
+
+    analyzeMessages = arcpy.CreateGPSDDraft(
+        result, sddraft, serviceName, server_type=serverType,
+        connection_file_path=connectionFilePath,
+        copy_data_to_server=copyDataToServer, folder_name=folderName,
+        summary=summary, tags=tags, executionType=executionType,
+        resultMapServer=resultMapServer, showMessages=showMessages, maximumRecords=maximumRecords,
+        minInstances=minInstances, maxInstances=maxInstances, maxUsageTime=maxUsageTime, maxWaitTime=maxWaitTime,
+        maxIdleTime=maxIdleTime, constantValues=constantValues
+    )
+    arcpy.AddMessage(arcpy.GetMessages(0))
+
+    # Stage and upload the service if the sddraft analysis did not
+    # contain errors
+    if analyzeMessages['errors'] == {}:
+        # Execute StageService
+        arcpy.StageService_server(sddraft, sd)
+        arcpy.AddMessage(arcpy.GetMessages(0))
+        # Execute UploadServiceDefinition
+        # Use URL to a federated server
+        arcpy.UploadServiceDefinition_server(sd, serverURL)
+        arcpy.AddMessage(arcpy.GetMessages(0))
+    else:
+        # If the sddraft analysis contained errors, display them
+        print(analyzeMessages['errors'])
+
+if __name__ == "__main__":
+    configFile = arcpy.GetParameterAsText(0)
+    CreateSDFile(configFile)
---
+++ Deployment Tools.pyt
@@ -0,0 +1,55 @@
+import arcpy
+
+class Toolbox(object):
+    def __init__(self):
+        """Define the toolbox (the name of the toolbox is the name of the
+        .pyt file)."""
+        self.label = "Deployment Tools"
+        self.alias = "deploy"
+
+        # List of tool classes associated with this toolbox
+        self.tools = [CreateSDFileTool]
+
+
+class CreateSDFileTool(object):
+    def __init__(self):
+        """Define the tool (tool name is the name of the class)."""
+        self.label = "Create SD File"
+        self.description = "Creates an SD file to a GP service"
+        self.canRunInBackground = False
+
+    def getParameterInfo(self):
+        """Define parameter definitions"""
+        params = None
+
+        configFile = arcpy.Parameter(
+            name="configFile",
+            displayName="Config File",
+            direction="Input",
+            datatype="DEFile",
+            parameterType="Required",
+        )
+
+        params = [configFile]
+        return params
+
+    def isLicensed(self):
+        """Set whether tool is licensed to execute."""
+        return True
+
+    def updateParameters(self, parameters):
+        """Modify the values and properties of parameters before internal
+        validation is performed.  This method is called whenever a parameter
+        has been changed."""
+        return
+
+    def updateMessages(self, parameters):
+        """Modify the messages created by internal validation for each tool
+        parameter.  This method is called after internal validation."""
+        return
+
+    def execute(self, parameters, messages):
+        """The source code of the tool."""
+        import CreateSDFile
+        CreateSDFile.CreateSDFile(parameters[0].valueAsText)
+        return
